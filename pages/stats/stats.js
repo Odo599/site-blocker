c4a0b418d7f8de945fc0ecd8bd5bf1fe3ba25914
odo599 <owen00064@gmail.com>
2025-06-29 21:04:15 +1000

    Moved stats page script inside DOMContentLoaded event listener

--- pages/stats/stats.js
+++ pages/stats/stats.js
@@ -1,40 +1,43 @@
 import {
     convertStatsToDictWithAmounts,
     getStats,
+    resetStats,
 } from "../../scripts/convertStats.mjs";
 
-const statsUl = document.querySelector("#stats");
-const resetButton = document.querySelector("#reset-button");
+document.addEventListener("DOMContentLoaded", () => {
+    const statsUl = document.querySelector("#stats");
+    const resetButton = document.querySelector("#reset-button");
 
-function updateStats() {
-    let stats = convertStatsToDictWithAmounts(getStats());
+    function updateStats() {
+        let stats = convertStatsToDictWithAmounts(getStats());
 
-    statsUl.innerHTML = "";
+        statsUl.innerHTML = "";
 
-    stats.forEach((value, key) => {
-        let li = document.createElement("li");
-        let nameSpan = document.createElement("span");
-        let countSpan = document.createElement("span");
+        stats.forEach((value, key) => {
+            let li = document.createElement("li");
+            let nameSpan = document.createElement("span");
+            let countSpan = document.createElement("span");
 
-        nameSpan.textContent = key;
-        countSpan.textContent = value;
+            nameSpan.textContent = key;
+            countSpan.textContent = value;
 
-        nameSpan.classList.add("stats-label");
-        countSpan.classList.add("stats-value");
+            nameSpan.classList.add("stats-label");
+            countSpan.classList.add("stats-value");
 
-        li.appendChild(nameSpan);
-        li.appendChild(countSpan);
-        statsUl.appendChild(li);
-    });
-}
+            li.appendChild(nameSpan);
+            li.appendChild(countSpan);
+            statsUl.appendChild(li);
+        });
+    }
 
-updateStats();
-
-resetButton.addEventListener("click", () => {
-    resetStats();
     updateStats();
-});
 
-window.addEventListener("storage", (e) => {
-    if (e.key === "stats") updateStats();
+    resetButton.addEventListener("click", () => {
+        resetStats();
+        updateStats();
+    });
+
+    window.addEventListener("storage", (e) => {
+        if (e.key === "stats") updateStats();
+    });
 });
